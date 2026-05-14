2a876cea3759e302031b2806f2c02defb8b6d5ff
kolom <kolomytseva.ev05@gmail.com>
2026-05-14 18:54:17 +0300

    Add laba1 and laba2

---
+++ k8s-rest-app/app/app.py
@@ -0,0 +1,39 @@
+﻿from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    name = os.getenv('NAME', 'Guest')
+    return f'''
+    <!DOCTYPE html>
+    <html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <title>Hello App</title>
+        <style>
+            body {{
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 100vh;
+                margin: 0;
+                font-family: Arial, sans-serif;
+                background-color: #f4f4f9;
+            }}
+            h1 {{ color: #2c3e50; }}
+        </style>
+    </head>
+    <body>
+        <h1>Hello {name}!</h1>
+    </body>
+    </html>
+    '''
+
+@app.route('/health')
+def health():
+    return 'OK', 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
